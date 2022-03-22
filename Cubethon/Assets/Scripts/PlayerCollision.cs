723ebc692b1c7a76d990b92b900d7d2eb34a0b05
c-cintron <CINTROC3@my.erau.edu>
2022-03-21 20:14:04 -0700

    done

--- Cubethon/Assets/Scripts/PlayerCollision.cs
+++ Cubethon/Assets/Scripts/PlayerCollision.cs
@@ -10,6 +10,7 @@ public class PlayerCollision : MonoBehaviour
         if (collision.gameObject.tag == "Obstacle")
         {
             movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
         }
     }
 }
